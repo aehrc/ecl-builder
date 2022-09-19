c486f3bee1336c4a483bc43bf2c2033d93c19c45
John Grimes <John.Grimes@csiro.au>
2022-09-19 14:01:30 +1000

    Make expression able to grow beyond the limits of the screen

--- apps/ecl-builder-app/src/App.tsx
+++ apps/ecl-builder-app/src/App.tsx
@@ -1,9 +1,22 @@
-import { Box, createTheme, Grid, ThemeProvider } from "@mui/material";
+import { Box, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import ExpressionBuilder from "ecl-builder";
 import ExpressionResult from "ecl-builder/lib/components/ExpressionResult";
 import React, { useState } from "react";
 
-const theme = createTheme();
+let theme = createTheme();
+theme = createTheme(theme, {
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          "& #root": {
+            display: "flex",
+          },
+        },
+      },
+    },
+  },
+});
 
 function App() {
   const [currentExpression, setCurrentExpression] =
@@ -11,30 +24,32 @@ function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <CssBaseline />
       <Box
-        component="main"
-        sx={(theme) => ({
-          backgroundColor: theme.palette.background.default,
-          height: "100vh",
+        className="app-container"
+        sx={{
+          display: "flex",
           p: 8,
+          gap: 4,
+          flexWrap: "wrap",
+          flexDirection: "row",
+          flexGrow: 1,
           [theme.breakpoints.down("lg")]: {
             p: 2,
           },
-        })}
+        }}
       >
-        <Grid container spacing={4} columnSpacing={12}>
-          <Grid item xs={12} xl={8} sx={{ overflow: "hidden" }}>
-            <ExpressionBuilder
-              expression={currentExpression}
-              onChange={setCurrentExpression}
-            />
-          </Grid>
-          {currentExpression ? (
-            <Grid item xs={12} xl={4}>
-              <ExpressionResult expression={currentExpression} />
-            </Grid>
-          ) : null}
-        </Grid>
+        <Box className="expression-builder-container" sx={{ flexGrow: 1 }}>
+          <ExpressionBuilder
+            expression={currentExpression}
+            onChange={setCurrentExpression}
+          />
+        </Box>
+        {currentExpression ? (
+          <Box className="expression-result-container" sx={{ flexGrow: 1 }}>
+            <ExpressionResult expression={currentExpression} />
+          </Box>
+        ) : null}
       </Box>
     </ThemeProvider>
   );
