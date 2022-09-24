c57007dc5dfe859e5cac796055ad618111b4d941
John Grimes <John.Grimes@csiro.au>
2022-09-24 07:08:55 +0100

    Use self and descendants by default on BlankExpression

--- packages/ecl-builder/src/components/ecl/BlankExpression.tsx
+++ packages/ecl-builder/src/components/ecl/BlankExpression.tsx
@@ -3,17 +3,33 @@
  * Organisation (CSIRO) ABN 41 687 119 230. All rights reserved.
  */
 
-import React from "react";
+import React, { useState } from "react";
 import { ChangeReporterProps } from "./ExpressionVisitor";
 import ConceptReference from "./sub/ConceptReference";
+import ConstraintOperator, {
+  constraintNameToOperator,
+  ConstraintType,
+  operatorToConstraintName,
+} from "./sub/ConstraintOperator";
 import SubExpression from "./sub/SubExpression";
 
 export type BlankExpressionProps = ChangeReporterProps;
 
 export default function BlankExpression({ onChange }: BlankExpressionProps) {
+  const [constraint, setConstraint] =
+    useState<ConstraintType>("descendantorselfof");
+
+  function handleChange(newExpression: string): void {
+    onChange(`${constraintNameToOperator[constraint]} ${newExpression}`);
+  }
+
   return (
     <SubExpression hideActions>
-      <ConceptReference onChange={onChange} />
+      <ConstraintOperator
+        constraint={constraint}
+        onChange={(e) => setConstraint(operatorToConstraintName[e])}
+      />
+      <ConceptReference onChange={handleChange} />
     </SubExpression>
   );
 }
